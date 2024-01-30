0e15d40968a733e493446844769f069afd5833e4
l3miage-delagral <alex.del57640@gmail.com>
2024-01-30 11:26:48 +0100

    update panier pour get by user + eltDefinition avec les tables

--- src/app/shared/services/eltDefinitions.ts
+++ src/app/shared/services/eltDefinitions.ts
@@ -24,5 +24,55 @@ export interface Panier {
   id: string,
   date: Date,
   idProprietaire: string,
-  nomFestivaliers: string[]
+  nomFestivaliers: string[] // ex: "Arthur - Alex - Kyks - Vincent" à split pour faire une liste
+  nbPlaceOccuppee: number,
+}
+
+export interface Etape {
+  id: number,
+  lieu: Lieu,
+  covoiturage: Covoiturage,
+  dureeDepuisDepart: number,
+  prixEtape: number,
+}
+
+export interface Covoiturage {
+  id: number,
+  dateDepart: Date,
+  nbPlace: number,
+  nbPlaceDispo: number,
+  marque: string,
+  modele: string,
+  couleur: string,
+  prixParPersonne: number,
+  conducteur: FestiUser,
+  festival: Festival,
+}
+
+export interface Lieu {
+  id: number,
+  nom: string,
+  adresse: string,
+  codeINSEE: string,
+  latitude: number,
+  longitude: number,
+  typeLieu: string,
+  commune: Commune
+}
+
+export interface Commune {
+  id: number,
+  codeINSEE: string,
+  codePostal: string,
+  latitude: number,
+  longitude: number,
+  nom: string,
+  departement: Departement
+}
+
+export interface Departement {
+  id: number,
+  code: string,
+  nom: string,
+  region: string
 }
